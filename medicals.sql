492e8e950b87d1e9141401ee629b438643ea652f
Immaculate <chepkiruiimmaculatekogo@gmail.com>
2025-05-12 19:51:56 +0300

    created medical_records table

---
+++ medicals.sql
@@ -0,0 +1,82 @@
+INSERT INTO children_vaccination.medical_records(medicalid,childid,pre_existing_condition,date_diagonised)VALUES
+(002,002,'kwarshiorkor','2-3-2025')
+
+SELECT * FROM children_vaccination.medical_records
+
+
+INSERT INTO children_vaccination.medical_records(medicalid,childid,pre_existing_condition,date_diagonised)VALUES
+(003,003,'Pneumonia','8-3-2025')
+
+SELECT * FROM children_vaccination.medical_records
+
+INSERT INTO children_vaccination.medical_records(medicalid,childid,pre_existing_condition,date_diagonised)VALUES
+(004,004,'Malaria','6/4/2025');
+
+SELECT * FROM children_vaccination.medical_records
+
+
+INSERT INTO children_vaccination.medical_records(medicalid,childid,pre_existing_condition,date_diagonised)VALUES
+(005,005,'Anaemia','3/4/2025');
+
+SELECT * FROM children_vaccination.medical_records
+
+
+INSERT INTO children_vaccination.medical_records(medicalid,childid,pre_existing_condition,date_diagonised)VALUES
+(006,006,'Flu','7/4/2025');
+
+SELECT * FROM children_vaccination.medical_records
+
+
+
+INSERT INTO children_vaccination.medical_records(medicalid,childid,pre_existing_condition,date_diagonised)VALUES
+(007,007,'headache','8/4/2025');
+
+SELECT * FROM children_vaccination.medical_records
+
+
+INSERT INTO children_vaccination.medical_records(medicalid,childid,pre_existing_condition,date_diagonised)VALUES
+(008,008,'marasmus','1/4/2025');
+
+SELECT * FROM children_vaccination.medical_records
+
+
+INSERT INTO children_vaccination.medical_records(medicalid,childid,pre_existing_condition,date_diagonised)VALUES
+(009,009,'Malaria','1/4/2025');
+
+SELECT * FROM children_vaccination.medical_records
+
+
+INSERT INTO children_vaccination.medical_records(medicalid,childid,pre_existing_condition,date_diagonised)VALUES
+(010,010,'blotting','9/4/2025');
+
+SELECT * FROM children_vaccination.medical_records
+
+
+INSERT INTO children_vaccination.medical_records(medicalid,childid,pre_existing_condition,date_diagonised)VALUES
+(011,011,'diabetes','16/4/2025');
+
+SELECT * FROM children_vaccination.medical_records
+
+
+INSERT INTO children_vaccination.medical_records(medicalid,childid,pre_existing_condition,date_diagonised)VALUES
+(012,012,'Tuberclosis','19/4/2025');
+
+SELECT * FROM children_vaccination.medical_records
+
+
+INSERT INTO children_vaccination.medical_records(medicalid,childid,pre_existing_condition,date_diagonised)VALUES
+(013,013,'stomachache','1/4/2025');
+
+SELECT * FROM children_vaccination.medical_records
+
+
+INSERT INTO children_vaccination.medical_records(medicalid,childid,pre_existing_condition,date_diagonised)VALUES
+(014,014,'cold','1/4/2025');
+
+SELECT * FROM children_vaccination.medical_records
+
+
+INSERT INTO children_vaccination.medical_records(medicalid,childid,pre_existing_condition,date_diagonised)VALUES
+(015,015,'arthritis','1/4/2025');
+
+SELECT * FROM children_vaccination.medical_recordspoooooooooo
