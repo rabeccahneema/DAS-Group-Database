56bea4e343d2b44513585634e82875ef55c422be
Rabecah <rabeccahneemah@gmail.com>
2025-05-12 21:07:28 +0300

    added fles

---
+++ table.sql
@@ -0,0 +1,67 @@
+-- Active: 1747030310990@@127.0.0.1@5432@data_class
+INSERT INTO children_vaccination.vaccine_details(vaccinationtype,supplier,manufacturingdate,expirydate)VALUES
+('measles','jane Kelvins','04-01-2025','4-27-2025')
+
+SELECT * FROM children_vaccination.vaccine_details
+
+INSERT INTO children_vaccination.vaccine_details(vaccinationtype,supplier,manufacturingdate,expirydate)VALUES
+('measles',' Kelvins','2025-04-10','2025-04-27')
+
+
+SELECT * FROM children_vaccination.vaccine_details
+
+
+INSERT INTO children_vaccination.vaccine_details(vaccinationtype,supplier,manufacturingdate,expirydate)VALUES
+('polio',' Metty','2025-03-10','2025-03-27')
+
+
+SELECT * FROM children_vaccination.vaccine_details
+
+INSERT INTO children_vaccination.vaccine_details(vaccinationtype,supplier,manufacturingdate,expirydate)VALUES
+('TB',' Becky','2025-02-10','2025-02-27')
+
+
+SELECT * FROM children_vaccination.vaccine_details
+
+
+INSERT INTO children_vaccination.vaccine_details(vaccinationtype,supplier,manufacturingdate,expirydate)VALUES
+('polio',' Magy','2025-03-10','2025-03-27')
+
+
+SELECT * FROM children_vaccination.vaccine_details
+
+INSERT INTO children_vaccination.vaccine_details(vaccinationtype,supplier,manufacturingdate,expirydate)VALUES
+('TB',' Tony','2025-02-10','2025-02-27')
+
+
+SELECT * FROM children_vaccination.vaccine_details
+
+INSERT INTO children_vaccination.vaccine_details(vaccinationtype,supplier,manufacturingdate,expirydate)VALUES
+('TB',' Henry','2025-02-10','2025-02-27')
+
+
+SELECT * FROM children_vaccination.vaccine_details
+
+INSERT INTO children_vaccination.vaccine_details(vaccinationtype,supplier,manufacturingdate,expirydate)VALUES
+('measles',' Hanetty','2025-04-10','2025-04-27')
+
+
+SELECT * FROM children_vaccination.vaccine_details
+
+INSERT INTO children_vaccination.vaccine_details(vaccinationtype,supplier,manufacturingdate,expirydate)VALUES
+('TB',' Willy','2025-02-10','2025-02-27')
+
+
+SELECT * FROM children_vaccination.vaccine_details
+
+INSERT INTO children_vaccination.vaccine_details(vaccinationtype,supplier,manufacturingdate,expirydate)VALUES
+('polio',' Shaddot','2025-03-10','2025-03-27')
+
+
+SELECT * FROM children_vaccination.vaccine_details
+
+INSERT INTO children_vaccination.vaccine_details(vaccinationtype,supplier,manufacturingdate,expirydate)VALUES
+('TB',' Kandie','2025-02-10','2025-02-27')
+
+
+SELECT * FROM children_vaccination.vaccine_details
