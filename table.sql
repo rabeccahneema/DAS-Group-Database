cef620212639a61c30030dc044f6f16659754e9f
kisanetshshay <shshaykisanet@gmail.com>
2025-05-12 21:02:08 +0300

    Created child_details

--- table.sql
+++ table.sql
@@ -1,1 +1,62 @@
--- Active: 1747023134431@@127.0.0.1@5432@data_class
+INSERT INTO children_vaccination.child_details(child_name,gender,date_of_birth,blood_type)values
+('Kisanet','f','2024-03-24','o')
+
+SELECT * FROM children_vaccination.child_details
+INSERT INTO children_vaccination.child_details(child_name,gender,date_of_birth,blood_type)values
+('Rebeca','F','2024-03-05','A')
+
+SELECT * FROM children_vaccination.child_details
+INSERT INTO children_vaccination.child_details(child_name,gender,date_of_birth,blood_type)values
+('John','M','2024-03-24','o')
+
+SELECT * FROM children_vaccination.child_details
+INSERT INTO children_vaccination.child_details(child_name,gender,date_of_birth,blood_type)values
+('Shaline','F','2023-03-05','A')
+
+SELECT * FROM children_vaccination.child_details
+
+INSERT INTO children_vaccination.child_details(child_name,gender,date_of_birth,blood_type)values
+('Emebet','F','2024-03-24','o')
+
+SELECT * FROM children_vaccination.child_details
+INSERT INTO children_vaccination.child_details(child_name,gender,date_of_birth,blood_type)values
+('Jennifer','F','2023-03-05','A')
+
+SELECT * FROM children_vaccination.child_details
+INSERT INTO children_vaccination.child_details(child_name,gender,date_of_birth,blood_type)values
+('Jecinta','F','2022-05-21','B')
+
+SELECT * FROM children_vaccination.child_details
+INSERT INTO children_vaccination.child_details(child_name,gender,date_of_birth,blood_type)values
+('Angela','F','2022-05-21','B')
+
+SELECT * FROM children_vaccination.child_details
+INSERT INTO children_vaccination.child_details(child_name,gender,date_of_birth,blood_type)values
+('Mercy','F','2022-05-21','B')
+
+SELECT * FROM children_vaccination.child_details
+INSERT INTO children_vaccination.child_details(child_name,gender,date_of_birth,blood_type)values
+('Bonnete','F','2022-03-24','A')
+
+SELECT * FROM children_vaccination.child_details
+
+INSERT INTO children_vaccination.child_details(child_name,gender,date_of_birth,blood_type)values
+('Micheal','M','2022-01-03','o')
+
+SELECT * FROM children_vaccination.child_details
+INSERT INTO children_vaccination.child_details(child_name,gender,date_of_birth,blood_type)values
+('Joni','M','2023-03-05','A')
+
+SELECT * FROM children_vaccination.child_details
+INSERT INTO children_vaccination.child_details(child_name,gender,date_of_birth,blood_type)values
+('Yoni','M','2024-06-08','B')
+
+SELECT * FROM children_vaccination.child_details
+INSERT INTO children_vaccination.child_details(child_name,gender,date_of_birth,blood_type)values
+('Qefar','M','2023-09-23','o')
+
+SELECT * FROM children_vaccination.child_details
+INSERT INTO children_vaccination.child_details(child_name,gender,date_of_birth,blood_type)values
+('Eyeru','F','2022-09-25','A')
+
+SELECT * FROM children_vaccination.child_detailspoooooooooo-- Active: 1747023134431@@127.0.0.1@5432@data_class
