355bbf70f52fd95188195061ec451587d169ffac
Immaculate <chepkiruiimmaculatekogo@gmail.com>
2025-05-19 15:20:55 +0300

    created parent_records table

--- medicals.sql
+++ medicals.sql
@@ -79,4 +79,42 @@ SELECT * FROM children_vaccination.medical_records
 INSERT INTO children_vaccination.medical_records(medicalid,childid,pre_existing_condition,date_diagonised)VALUES
 (015,015,'arthritis','1/4/2025');
 
-SELECT * FROM children_vaccination.medical_recordspoooooooooo
+SELECT * FROM children_vaccination.medical_records
+
+
+
+
+
+
+
+
+
+
+
+create table Children_Vaccination.Parent_details(
+Parent_ID SERIAL primary key,
+Name VARCHAR ( 25) not null,
+Gender VARCHAR (10) not null check (Gender in ('Female','Male')),
+national_ID VARCHAR (20) not null 
+);
+INSERT INTO Children_Vaccination.parent_details(Name,Gender, national_ID) values 
+('Mercy', 'Female', '39391630'),
+('Edith', 'Female', '40456090'),
+('Becky', 'Female', '22601111'),
+('JOY','Female','123456789'),
+('Mutai','Male','9876543'),
+('Mwikali','Female','8765432'),
+('John', 'Male','2345678'),
+('james', 'Male','3456789'),
+('Bonnet','Female','4568934'),
+('Faith','Female','2345678'),
+('Jude','Male','34578239'),
+('Mercy', 'Female', '39391630'),
+('Mercy', 'Female', '39391630'),
+('Becky', 'Female', '22601111'),
+('Becky', 'Female', '22601111');
+
+
+
+SELECT * FROM Children_Vaccination.parent_details;
+
